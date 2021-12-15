ec1547e0bad24e9c65098f8c4c144d525306d12b
Md Mazdi Hasan <md.mazdi@gmail.com>
2021-12-16 03:10:35 +0600

    remove usecreateindex

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import db from "../../utils/db"
+db.dbConnect();
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
